903b0f7dd708fbc5069f351acc8fadff6ff096ed
madison-porter <mnporter12@gmail.com>
2023-08-18 09:55:12 -0700

    updates to website

---
+++ js/index.js
@@ -0,0 +1,66 @@
+$("#about").on({mouseenter: function (){
+    $("#about").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ ("#about").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+$("#work").on({mouseenter: function (){
+    $("#work").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ ("#resume").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+$("#contact").on({mouseenter: function (){
+    $("#contact").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ ("#contact").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+
+$(".btn").on({mouseenter: function (){
+    $(".btn").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ (".btn").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+
+$(".navbar-brand").on({mouseenter: function (){
+    $(".navbar-brand").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ (".navbar-brand").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
